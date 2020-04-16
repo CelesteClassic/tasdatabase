32e44b6be29cac5ab71c2e909479eaca681d9179
AnInternetTroll <lucafulger@gmail.com>
2020-04-16 18:30:23 +0200

    Added more categories to the database

---
+++ database.js
@@ -0,0 +1,18 @@
+var url = document.URL.split("/");
+var category = url[url.length-2];
+var game = url[url.length-3];
+let database;
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "../../database.json");
+xhr.send();
+xhr.onload = () => {
+    database = JSON.parse(xhr.responseText);
+    // Format is database['game']['category']
+    // If unsure check out database.json
+    database = database[game][category.toLowerCase()];
+    for(i = 0;i < database.length;i++){
+        document.getElementById("tableStuff").innerHTML += 
+            '<tr><td><a href='+database[i]['file']+' download>'+database[i]['name']+'</a></td>'+
+            '<td>'+database[i]['frames']+'f</td></tr>';
+    }
+}
