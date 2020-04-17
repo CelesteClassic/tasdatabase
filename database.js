3f57b5d091a0246c31180bde1fde8cce42f8f367
AnInternetTroll <lucafulger@gmail.com>
2020-04-17 13:47:02 +0200

    Better support for un-tased levels

--- database.js
+++ database.js
@@ -13,13 +13,13 @@ xhr.onload = () => {
     if (category.toLocaleLowerCase().includes('dash')){
         for (i = 0; i < database.length; i++) {
             document.getElementById("tableStuff").innerHTML +=
-                '<tr><td><a href=' + database[i]['file'] + ' download>' + database[i]['name'] + '</a></td>' +
-                '<td>' + database[i]['frames'] + 'f</td><td>' + database[i]["dashes"] + 'd</tr>';
+                '<tr><td>'+(database[i]['file'] == null?"":'<a href=' + database[i]['file'] + ' download>')+ database[i]['name'] + '</a></td>' +
+                '<td>' + (database[i]['frames'] == null?"":database[i]['frames']+'f') + 'f</td><td>' + database[i]["dashes"] + 'd</tr>';
         }
     } else {
         for (i = 0; i < database.length; i++) {
             document.getElementById("tableStuff").innerHTML +=
-                '<tr><td><a href=' + database[i]['file'] + ' download>' + database[i]['name'] + '</a></td>' +
+            '<tr><td>'+(database[i]['file'] == null?"":'<a href=' + database[i]['file'] + ' download>')+ database[i]['name'] + '</a></td>' +
                 '<td>' + (database[i]['frames'] == null?"":database[i]['frames']+'f')+'</td></tr>';
         }
     }
