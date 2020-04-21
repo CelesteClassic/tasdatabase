8721ae02dffa3360c13884ee27398459b3a76236
AnInternetTroll <lucafulger@gmail.com>
2020-04-21 14:42:50 +0200

    Cleaned up files Loading text added Total dashes counter automated

--- database.js
+++ database.js
@@ -1,13 +1,23 @@
+var loadingEl = document.createElement("p");
+var loadingText = document.createTextNode("Loading...");
+loadingEl.appendChild(loadingText);
+document.getElementById("databaseTable").appendChild(loadingEl);
 var url = document.URL.split("/");
 var category = url[url.length - 2];
 var game = url[url.length - 3];
+let totalDash = 0; // :roundelie:
+let totalJump = 0;
+let totalLeftPress = 0;
+let totalRightPress = 0;
 let database;
 let xhr = new XMLHttpRequest();
+let xhr2 = new XMLHttpRequest();
 function timeTas(file) {
     if (file == null) {
         return null
     } else {
-        let xhr2 = new XMLHttpRequest();
+        // This makes a synchous call
+        // Bad idea, don't know how to fix
         xhr2.open("GET", file, false);
         xhr2.send();
         let tas;
@@ -22,16 +32,16 @@ function timeTas(file) {
             dash = Math.floor(x / 32) % 2;
             jump = Math.floor(x / 16) % 2;
             */
-            if (x&16){
+            if (x & 16) {
                 jumps++;
             }
-            if (x&32){
+            if (x & 32) {
                 dashes++;
             }
-            if (x&2){
+            if (x & 2) {
                 leftPress++;
             }
-            if (x&1){
+            if (x & 1) {
                 rightPress++;
             }
         });
@@ -54,6 +64,7 @@ xhr.onload = () => {
             document.getElementById("databaseTable").innerHTML +=
                 '<tr><td>' + (database[i]['file'] == null ? "" : '<a href=' + database[i]['file'] + ' download>') + database[i]['name'] + '</a></td>' +
                 '<td>' + (database[i]['file'] == null ? "" : tas[0] + 'f') + '</td><td>' + tas[1] + 'd</tr>';
+            totalDash += tas[1];
         }
     } else {
         for (i = 0; i < database.length; i++) {
@@ -62,5 +73,15 @@ xhr.onload = () => {
                 '<tr><td>' + (database[i]['file'] == null ? "" : '<a href=' + database[i]['file'] + ' download>') + database[i]['name'] + '</a></td>' +
                 '<td>' + (database[i]['file'] == null ? "" : tas[0] + 'f') + '</td></tr>';
         }
+    }
+}
+xhr2.onreadystatechange = function () {
+    loadingEl.style.display = 'none';
+    //document.getElementById('category').innerHTML = category;
+    if (category.toLocaleLowerCase().includes('dash')) {
+        setTimeout(function () {
+            //do what you need here
+            document.getElementById('totalDashes').innerHTML = totalDash;
+        }, 1);
     }
 }
